14935a05e91cea14e0fa7497e9e691e02a2cd0bf
juicexxtt <vallewilfredo101@gmail.com>
2025-05-16 08:57:36 -0600

    agregar conecciones en DataAcces

---
+++ DataAccess/conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security.Permissions;
+
+namespace TiendaVideojuegos.DataAccess
+{
+    internal class conexion
+    {
+        //Conexion base de datos 
+        private static string connectionString = "Server=localhost; Database=; UserId =; Password=;TrustServerCertificate=True";
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
