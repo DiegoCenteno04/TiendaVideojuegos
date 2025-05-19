0e2fc121320ef9a3c549442a87c567db4bf1bc8c
juicexxtt <vallewilfredo101@gmail.com>
2025-05-18 18:47:40 -0600

    Se Creo el forms Productos

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace TiendaVideojuegos.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _49754 {
+            get {
+                object obj = ResourceManager.GetObject("49754", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap juegos {
             get {
                 object obj = ResourceManager.GetObject("juegos", resourceCulture);
---
+++ View/Productos.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace TiendaVideojuegos.View
+{
+    partial class Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Productos));
+            panel1 = new Panel();
+            button1 = new Button();
+            panel2 = new Panel();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            label3 = new Label();
+            label8 = new Label();
+            label4 = new Label();
+            label7 = new Label();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkRed;
+            panel1.BackgroundImage = Properties.Resources._49754;
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(-2, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1289, 109);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Location = new Point(3, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 106);
+            button1.TabIndex = 8;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkRed;
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label7);
+            panel2.Location = new Point(-2, 98);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(524, 539);
+            panel2.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(229, 408);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(265, 27);
+            textBox6.TabIndex = 47;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(229, 332);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(265, 27);
+            textBox5.TabIndex = 46;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(229, 256);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(265, 27);
+            textBox4.TabIndex = 45;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(229, 190);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(265, 27);
+            textBox3.TabIndex = 44;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(229, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(265, 27);
+            textBox2.TabIndex = 43;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(229, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(265, 27);
+            textBox1.TabIndex = 42;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.Black;
+            label10.Location = new Point(28, 407);
+            label10.Name = "label10";
+            label10.Size = new Size(157, 24);
+            label10.TabIndex = 41;
+            label10.Text = "IdPlataforma: ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(28, 331);
+            label9.Name = "label9";
+            label9.Size = new Size(143, 24);
+            label9.TabIndex = 40;
+            label9.Text = "IdCategoria: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(14, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(147, 24);
+            label3.TabIndex = 36;
+            label3.Text = "IdProductos: ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.White;
+            label8.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(28, 259);
+            label8.Name = "label8";
+            label8.Size = new Size(150, 24);
+            label8.TabIndex = 39;
+            label8.Text = "IdProveedor: ";
+            label8.Click += label8_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(3, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(200, 24);
+            label4.TabIndex = 37;
+            label4.Text = "Nombre Producto: ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(3, 120);
+            label7.Name = "label7";
+            label7.Size = new Size(216, 24);
+            label7.TabIndex = 38;
+            label7.Text = "IdEstado Producto : ";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(686, 152);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(520, 383);
+            dataGridView1.TabIndex = 2;
+            // 
+            // Productos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1285, 631);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "Productos";
+            Text = "Productos";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Panel panel2;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label10;
+        private Label label9;
+        private Label label3;
+        private Label label8;
+        private Label label4;
+        private Label label7;
+        private DataGridView dataGridView1;
+    }
+}
